067d80f109d1601e3fe7722a41c16a48e3ae6483
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-09-12 14:23:35 +0300

    Initial Commit

---
+++ WebsocketsMQTT POC/WebsocketsMQTT_POCApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebsocketsMQTT_POCApp.swift
+//  WebsocketsMQTT POC
+//
+//  Created by Sylvan  on 12/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct WebsocketsMQTT_POCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
